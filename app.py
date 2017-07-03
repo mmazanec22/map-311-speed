11633c105ddab4f1db1c616c0ce2521b034a077a
mmazanec22 <melanie.mazanec@gmail.com>
2017-07-03 10:18:44 -0500

    Start to get form im place

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ import dateutil.parser
 import requests
 
 from datetime import datetime
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 
 # API DOCS
 # graffiti removal
 # https://dev.socrata.com/foundry/data.cityofchicago.org/cdmx-wzbz
-graffiti_removal = 'https://data.cityofchicago.org/resource/cdmx-wzbz.json?$limit=1000'
 # abandoned vehicles
 # https://dev.socrata.com/foundry/data.cityofchicago.org/suj7-cg3j
-abandoned_vehicles = 'https://data.cityofchicago.org/resource/suj7-cg3j.json?$limit=1000'
 # rodent baiting
 # https://dev.socrata.com/foundry/data.cityofchicago.org/dvua-vftq
-rodent_baiting = 'https://data.cityofchicago.org/resource/dvua-vftq.json'
 # potholes
 # https://dev.socrata.com/foundry/data.cityofchicago.org/787j-mys9
-potholes = 'https://data.cityofchicago.org/resource/787j-mys9.json?$limit=1000'
 
-dataset_names = ['graffiti_removal', 'abandoned_vehicles', 'rodent_baiting', 'potholes']
+datasets_dict = {
+    'graffiti_removal': 'https://data.cityofchicago.org/resource/cdmx-wzbz.json?$limit=1000',
+    'abandoned_vehicles': 'https://data.cityofchicago.org/resource/suj7-cg3j.json?$limit=1000',
+    'rodent_baiting': 'https://data.cityofchicago.org/resource/dvua-vftq.json',
+    'potholes': 'https://data.cityofchicago.org/resource/787j-mys9.json?$limit=1000',
+    }
 
 
 app = Flask(__name__)
@@ -27,6 +28,23 @@ app.config['DEBUG'] = True
 
 with app.app_context():
     url_for('static', filename='style.css')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    dataset_names = list(datasets_dict.keys())
+    dataset = dataset_names[0]
+    if request.method == 'POST':
+        dataset = request.form['which_dataset']
+    response = requests.get(datasets_dict.get(dataset))
+    if response.status_code == 200:
+        response_data = response.json()
+        response_data = [
+            {
+                'coordinates': [float(point.get('longitude')) if point.get('longitude') else 0, float(point.get('latitude')) if point.get('latitude') else 0],
+                'seconds_to_completion': seconds_to_completion(point.get('creation_date'), point.get('completion_date')),
+            }
+            for point in response_data]
+    return render_template('index.html', apiData=response_data, datasets=dataset_names, checked_set=dataset)
 
 
 def seconds_to_completion(startDate, endDate):
@@ -37,20 +55,6 @@ def seconds_to_completion(startDate, endDate):
     else:
         returnVal = (datetime.now() - startDate).total_seconds()
         return f'incomplete-{returnVal}'
-
-
-@app.route('/')
-def index():
-    response = requests.get(graffiti_removal)
-    if response.status_code == 200:
-        response_data = response.json()
-        response_data = [
-            {
-                'coordinates': [float(point.get('longitude')) if point.get('longitude') else 0, float(point.get('latitude')) if point.get('latitude') else 0],
-                'seconds_to_completion': seconds_to_completion(point.get('creation_date'), point.get('completion_date')),
-            }
-            for point in response_data]
-    return render_template('index.html', apiData=response_data, datasets=dataset_names)
 
 
 if __name__ == "__main__":
